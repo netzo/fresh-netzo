a0055375e95075a55ce8fed1a6100d4d5a185810
Miguel Romero Karam <miguelromerokaram@gmail.com>
2022-09-26 05:23:48 +0200

    refactor(openapi): removes deprecated openapi package

--- packages/openapi/create-client/get-caller.ts
+++
@@ -1,8 +0,0 @@
-import OAClientError from "../errors/OAClientError.ts";
-import { Callers, routeSpec } from "../types.ts";
-
-export default (callers: Callers, routeSpec: routeSpec, method: string) => {
-  const type = routeSpec["x-type"] ?? method;
-  if (!(type in callers)) throw new OAClientError(2, { type, callers });
-  return callers[type];
-};
--- packages/openapi/create-client/validate-request/validate-param.ts
+++
@@ -1,9 +0,0 @@
-import validate from "./validate.ts";
-import OAClientError from "../../errors/OAClientError.ts";
-
-export default (parameter: any, value: any) => {
-  if (!value && !parameter.required) return;
-  if (!value && parameter.required) throw new OAClientError(104, { parameter });
-  const schema = parameter.schema || parameter.content;
-  validate(schema, value);
-};
