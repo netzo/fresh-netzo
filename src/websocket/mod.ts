aebb50dfc23ac8ae663d39f32419a4b344e050fd
Miguel Romero Karam <miguelromerokaram@gmail.com>
2022-09-29 22:42:35 +0200

    wip(websocket): adds initial boilerplate

---
+++ src/websocket/mod.ts
@@ -0,0 +1,13 @@
+export interface ClientOptionsWebSocket { }
+
+export interface ClientWebSocket { }
+
+/**
+ * Minimal, type-safe WebSocket client using JS proxies
+ */
+export function websocket(
+  defaultOptions: ClientOptionsWebSocket
+): ClientWebSocket {
+  // TODO: implement websocket client using http client as base
+  return {}
+}
