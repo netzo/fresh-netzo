b1b74a4f339f5e0c566a495b43eb3276e66897a5
Miguel Romero Karam <miguelromerokaram@gmail.com>
2022-09-29 22:42:26 +0200

    wip(sse): adds initial boilerplate

---
+++ src/sse/mod.ts
@@ -0,0 +1,13 @@
+export interface ClientOptionsSSE { }
+
+export interface ClientSSE { }
+
+/**
+ * Minimal, type-safe SSE client using JS proxies
+ */
+export function sse(
+  defaultOptions: ClientOptionsSSE
+): ClientSSE {
+  // TODO: implement sse client using http client as base
+  return {}
+}
