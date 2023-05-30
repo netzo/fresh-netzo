a7d432d6c922d16a043f9025e89a3a0781da671f
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-05-30 05:14:33 +0200

    chore: update template.json

--- templates/sales-dashboard-fresh/src/routes/index.tsx
+++ templates/sales-dashboard-fresh/src/routes/index.tsx
@@ -5,7 +5,6 @@ import { asset, Head } from '$fresh/runtime.ts'
 import { Flex, Text, Title } from '@tremor/react'
 import Shell from '../islands/Shell.tsx'
 
-// Dashboard Settings
 const title = 'Sales Dashboard'
 const description =
   'A sales dashboard to track performance of sales teams in real-time, and provides a variety of metrics to help make informed decisions.'
@@ -14,26 +13,21 @@ export default () => {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{`${title} | Netzo`}</title>
+        <meta name='description' content={description} />
       </Head>
 
       <main className='p-6'>
         <Flex justifyContent='justify-between' spaceX='space-x-2'>
           <Flex justifyContent='justify-start'>
-            <svg className='mr-6' width='64' height='64' viewBox='0 0 24 24'>
-              <path
-                fill='currentColor'
-                d='M18 15h-2v2h2m0-6h-2v2h2m2 6h-8v-2h2v-2h-2v-2h2v-2h-2V9h8M10 7H8V5h2m0 6H8V9h2m0 6H8v-2h2m0 6H8v-2h2M6 7H4V5h2m0 6H4V9h2m0 6H4v-2h2m0 6H4v-2h2m6-10V3H2v18h20V7H12Z'
-              >
-              </path>
-            </svg>
+            <img src={asset('/logo.svg')} className='h-8' />
             <div>
               <Title>{title}</Title>
               <Text marginTop='mt-2'>{description}</Text>
             </div>
           </Flex>
           <a href='https://netzo.io' target='_blank'>
-            <img src={asset('/logo.svg')} className='h-8' />
+            <img src={asset('/built-with-netzo.svg')} className='h-8' />
           </a>
         </Flex>
 
