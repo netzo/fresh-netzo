736182d3f47f1d1cecf615b29b8316d0443d72bd
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-06-06 20:13:44 +0200

    chore: update deps

--- templates/sales-dashboard-fresh/src/main.ts
+++ templates/sales-dashboard-fresh/src/main.ts
@@ -11,7 +11,7 @@ import manifest from './fresh.gen.ts'
 
 import twindPlugin from '$fresh/plugins/twind.ts'
 import twindConfig from './twind.config.ts'
-import unocss from 'https://deno.land/x/netzo@v0.1.70/lib/plugins/unocss/mod.ts'
+import unocss from 'https://deno.land/x/netzo@v0.1.71/lib/plugins/unocss/mod.ts'
 
 await start(manifest, {
   plugins: [
