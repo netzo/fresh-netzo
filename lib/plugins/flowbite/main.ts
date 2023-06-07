65f00ac368f7a4a802e318d50060d0b5ff3a4c55
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-06-07 02:57:19 +0200

    chore: format codebase

--- lib/plugins/flowbite/main.ts
+++ lib/plugins/flowbite/main.ts
@@ -2,7 +2,9 @@ import { injectScript, injectStylesheet } from '../utils.ts'
 import { FlowbitePluginOptions } from './mod.ts'
 
 export default function main(state: FlowbitePluginOptions = {}) {
-  injectScript('https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js')
+  injectScript(
+    'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js',
+  )
   const { additionalScripts = [], additionalStylesheets = [] } = state
   additionalScripts.forEach((script) => injectScript(script))
   additionalStylesheets.forEach((stylesheet) => injectStylesheet(stylesheet))
--- lib/plugins/unocss/main.ts
+++ lib/plugins/unocss/main.ts
@@ -3,6 +3,8 @@ import { injectScript } from '../utils.ts'
 
 export default function main(options: Options = 'uno') {
   if (typeof options === 'string') {
-    injectScript(`https://cdn.jsdelivr.net/npm/@unocss/runtime/${options}.global.js`)
+    injectScript(
+      `https://cdn.jsdelivr.net/npm/@unocss/runtime/${options}.global.js`,
+    )
   } else if (typeof options === 'object') window.__unocss = options
 }
