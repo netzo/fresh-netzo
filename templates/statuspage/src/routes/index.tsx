25f276527a4673f2347dbbaad102d147fd52b913
Arturo Romero <arturo.ro.ka@gmail.com>
2023-06-29 23:30:37 +0200

    chore: add new icones

--- templates/statuspage/src/routes/index.tsx
+++ templates/statuspage/src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { Head } from '$fresh/runtime.ts'
-import Statuspage from '../islands/Statuspage.tsx'
+import { Head } from "$fresh/runtime.ts";
+import Statuspage from "../islands/Statuspage.tsx";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Fresh App</title>
-        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
       <Statuspage />
     </>
-  )
+  );
 }
