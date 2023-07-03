9bb049357f635f6eb5cccc9511b80cb198735cf8
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-03 14:30:09 +0200

    chore: format codebase

--- lib/apis/whatsappbusiness/mod.ts
+++ lib/apis/whatsappbusiness/mod.ts
@@ -9,7 +9,7 @@ import { auth } from '../create-api/auth/mod.ts'
  */
 export const whatsappbusiness = ({
   businessAccountId = Deno.env.get('WHATSAPPBUSINESS_BUSINESS_ACCOUNT_ID') ||
-  '',
+    '',
   permanentToken = Deno.env.get('WHATSAPPBUSINESS_PERMANENT_TOKEN'),
 }) => {
   const api = createApi({
