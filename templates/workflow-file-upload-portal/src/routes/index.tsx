a1d0beef39ef2cd1201b3c381fc3b284a768a43f
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-05 10:52:28 +0200

    chore: format codebase

--- templates/workflow-file-upload-portal/src/routes/index.tsx
+++ templates/workflow-file-upload-portal/src/routes/index.tsx
@@ -1,64 +1,64 @@
-import { Head } from "fresh/runtime.ts";
-import { PageProps } from "fresh/server.ts";
-import { Handlers } from "fresh/server.ts";
-import { signal } from "@preact/signals";
-import Form from "../islands/Form.tsx";
+import { Head } from 'fresh/runtime.ts'
+import { PageProps } from 'fresh/server.ts'
+import { Handlers } from 'fresh/server.ts'
+import { signal } from '@preact/signals'
+import Form from '../islands/Form.tsx'
 
 const meta = {
-  title: "File Upload Portal",
+  title: 'File Upload Portal',
   description:
-    "A file upload tool to ease data processing and syncing from files.",
-};
+    'A file upload tool to ease data processing and syncing from files.',
+}
 
 // see https://creatomate.com/blog/the-best-video-generation-apis
 
 export const handler: Handlers = {
   GET: async (_req, ctx) => {
-    return await ctx.render();
+    return await ctx.render()
   },
   POST: async (req, _ctx) => {
-    const form = await req.formData();
-    const email = form.get("email")?.toString();
-    const files = form.get("files")?.toString();
-    console.log({ email, files });
+    const form = await req.formData()
+    const email = form.get('email')?.toString()
+    const files = form.get('files')?.toString()
+    console.log({ email, files })
     // redirect to home page
-    const headers = new Headers();
-    headers.set("Location", "/");
-    return new Response(undefined, { status: 303, headers }); // 303: See Other
+    const headers = new Headers()
+    headers.set('Location', '/')
+    return new Response(undefined, { status: 303, headers }) // 303: See Other
   },
-};
+}
 
-const isLoading = signal(false);
+const isLoading = signal(false)
 
 export default (props: PageProps) => {
   return (
     <>
       <Head>
         <title>{`${meta.title} | Netzo`}</title>
-        <meta name="description" content={meta.description} />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name='description' content={meta.description} />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
       </Head>
 
-      <body class="flex flex-col">
-        <header class="flex justify-between items-center py-6 px-10">
+      <body class='flex flex-col'>
+        <header class='flex justify-between items-center py-6 px-10'>
           <div>
-            <h1 class="text-2xl mb-1 font-semibold dark:text-white">
+            <h1 class='text-2xl mb-1 font-semibold dark:text-white'>
               {meta.title}
             </h1>
-            <p class="text-sm dark:text-gray-300">{meta.description}</p>
+            <p class='text-sm dark:text-gray-300'>{meta.description}</p>
           </div>
-          <a href="https://netzo.io" target="_blank">
+          <a href='https://netzo.io' target='_blank'>
             <img
-              src="https://netzo.io/images/built-with-netzo-light.svg"
-              class="h-10"
+              src='https://netzo.io/images/built-with-netzo-light.svg'
+              class='h-10'
             />
           </a>
         </header>
 
-        <main class="flex-1">
+        <main class='flex-1'>
           <Form />
         </main>
       </body>
     </>
-  );
-};
+  )
+}
