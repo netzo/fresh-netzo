0705d0a92d7321d11a13b9dd457b4f509bf3bc77
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-08 15:29:27 +0200

    wip(lib): add demo for `netzo/lib/ui` module based on starter-fresh

---
+++ lib/ui/demo/main.ts
@@ -0,0 +1,19 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import 'std/dotenv/load.ts'
+
+import { start } from 'fresh/server.ts'
+import manifest from './fresh.gen.ts'
+
+import unocssPlugin from 'netzo/lib/plugins/unocss/mod.ts'
+import { presetNetzo } from 'netzo/lib/ui/mod.ts'
+
+await start(manifest, {
+  plugins: [
+    unocssPlugin({ presets: [presetNetzo()] }),
+  ],
+})
