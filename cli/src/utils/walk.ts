23284ca18f16db6b0020f3d7231266e81b9e6779
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-08 15:32:16 +0200

    feat(cli): skip multimedia files until supported (see `walk()`)

--- cli/src/utils/walk.ts
+++ cli/src/utils/walk.ts
@@ -1,4 +1,5 @@
 import { join, type ManifestEntry, ManifestEntryDirectory } from '../../deps.ts'
+import { printWarning } from "../console.ts";
 
 /** Calculate git object hash, like `git hash-object` does. */
 export async function calculateGitSha1(bytes: Uint8Array) {
@@ -19,6 +20,47 @@ function include(
   include?: string[],
   exclude?: string[],
 ): boolean {
+
+  // FIXME: remove first if case that exclude binary multimedia files
+  // (images, videos, etc.) and add support for them as well via the
+  // Subhosting API integration (see https://deno-deploy.redoc.ly)
+  // TODO: read .gitignore file and skip those paths instead (if any)
+  if (
+    [
+      'png',
+      'jpg',
+      'jpeg',
+      'webp',
+      'gif',
+      'ico',
+      'mp3',
+      'mp4',
+      'wav',
+      'ogg',
+      'pdf',
+      'doc',
+      'docx',
+      'xls',
+      'xlsx',
+      'ppt',
+      'pptx',
+      'zip',
+      'webm',
+      'ogg',
+      'm4a',
+      'm4v',
+      'mov',
+      'avi',
+      'wmv',
+      'mpg',
+      'mpeg',
+    ].includes(path?.split('.').pop()!)
+  ) {
+    printWarning(
+      `Skipping ${path} because it is a binary file (not yet supported)`,
+    )
+    return false
+  }
   if (
     include && !include.some((pattern): boolean => path.startsWith(pattern))
   ) {
