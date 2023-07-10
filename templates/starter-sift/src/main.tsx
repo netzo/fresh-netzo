d846bbd1f5f196a17b577faba9780fecd414a4e3
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-10 13:15:27 +0200

    fix(templates): set `"compilerOptions.jsxImportSource": "preact"` to use alias and update dependencies to latest

--- templates/starter-sift/src/main.tsx
+++ templates/starter-sift/src/main.tsx
@@ -1,19 +1,19 @@
-import { json, jsx, serve } from "https://deno.land/x/sift@0.6.0/mod.ts";
+import { json, jsx, serve } from 'https://deno.land/x/sift@0.6.0/mod.ts'
 
 const App = () => (
   <div>
     <h1>Hello World!</h1>
   </div>
-);
+)
 
 const NotFound = () => (
   <div>
     <h1>Page not found</h1>
   </div>
-);
+)
 
 serve({
-  "/": () => jsx(<App />),
-  "/api": () => json({ message: "Hello World!" }),
-  "404": () => jsx(<NotFound />, { status: 404 }),
-});
+  '/': () => jsx(<App />),
+  '/api': () => json({ message: 'Hello World!' }),
+  '404': () => jsx(<NotFound />, { status: 404 }),
+})
