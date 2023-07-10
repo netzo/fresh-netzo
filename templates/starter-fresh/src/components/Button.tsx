abf989af019d9072ab1a54c3105083e28cd299c3
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-10 13:08:14 +0200

    chore(templates): update starter-fresh to latest

--- templates/starter-fresh/src/components/Button.tsx
+++ templates/starter-fresh/src/components/Button.tsx
@@ -1,12 +1,11 @@
 import { JSX } from 'preact'
-import { IS_BROWSER } from 'fresh/runtime.ts'
+import { IS_BROWSER } from '$fresh/runtime.ts'
 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class='px-2 py-1 border(gray-100 2) hover:bg-gray-200'
     />
   )
 }
--- templates/starter-fresh/src/routes/index.tsx
+++ templates/starter-fresh/src/routes/index.tsx
@@ -1,24 +1,26 @@
-import { Head } from 'fresh/runtime.ts'
+import { Head } from '$fresh/runtime.ts'
+import { useSignal } from '@preact/signals'
 import Counter from '../islands/Counter.tsx'
 
 export default function Home() {
+  const count = useSignal(3)
   return (
     <>
       <Head>
         <title>Fresh App</title>
-        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
       </Head>
-      <div class='p-4 mx-auto max-w-screen-md'>
+      <div>
         <img
           src='/logo.svg'
-          class='w-32 h-32'
+          width='128'
+          height='128'
           alt='the fresh logo: a sliced lemon dripping with juice'
         />
-        <p class='my-6'>
+        <p>
           Welcome to `fresh`. Try updating this message in the
           ./routes/index.tsx file, and refresh.
         </p>
-        <Counter start={3} />
+        <Counter count={count} />
       </div>
     </>
   )
