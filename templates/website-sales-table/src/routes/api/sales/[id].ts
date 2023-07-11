ccdcc34693f94f888bb2889c12eda6e072fd87fe
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-11 18:07:27 +0200

    chore(templates): removes wip templates

--- templates/website-sales-table/src/routes/api/sales/[id].ts
+++
@@ -1,9 +0,0 @@
-import { HandlerContext, Handlers } from 'fresh/server.ts'
-import { sales } from './index.ts'
-
-export const handler: Handlers = {
-  GET(_req: Request, ctx: HandlerContext) {
-    const data = sales.filter(({ id }) => id === ctx.params.id)
-    return Response.json(data)
-  },
-}
