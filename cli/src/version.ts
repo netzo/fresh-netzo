5de1362c832d1a0c5a2d952f308792d3e0c8df3b
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-14 01:34:47 +0200

    0.1.95

--- cli/src/version.ts
+++ cli/src/version.ts
@@ -1,5 +1,5 @@
 // latest version of netzo/cli (see https://github.com/netzo/netzo/releases)
-export const VERSION = '0.1.94'
+export const VERSION = '0.1.95'
 
 // minimum version of Deno required to run this CLI
 // (see https://github.com/denoland/deployctl/blob/main/src/version.ts)
