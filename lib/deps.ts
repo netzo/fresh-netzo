e906f12e471158961666881a1d90ae77a0ab7257
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-19 10:07:48 +0200

    chore(templates): bump fresh to 1.3.0

--- lib/deps.ts
+++ lib/deps.ts
@@ -5,7 +5,7 @@
 export type {
   MiddlewareHandler,
   Plugin,
-} from 'https://deno.land/x/fresh@1.2.0/server.ts'
+} from 'https://deno.land/x/fresh@1.3.0/server.ts'
 
 // preact:
 export {
@@ -24,7 +24,7 @@ export type { Variable } from 'https://esm.sh/@netzo/api@1.0.30'
 export { get } from 'https://esm.sh/radash@11.0.0'
 
 // @headlessui/react:
-export {} from 'https://esm.sh/@headlessui/react@1.7.14?alias=react:preact/compat,react-dom:preact/compat,@types/react:preact/compat&external=preact/compat'
+export { } from 'https://esm.sh/@headlessui/react@1.7.14?alias=react:preact/compat,react-dom:preact/compat,@types/react:preact/compat&external=preact/compat'
 
 // shiki-es:
 export {
