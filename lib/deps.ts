12dd2685d6e0a9204c744138e93ffe8c19d8f61c
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-19 10:37:11 +0200

    chore: format codebase

--- lib/deps.ts
+++ lib/deps.ts
@@ -24,7 +24,7 @@ export type { Variable } from 'https://esm.sh/@netzo/api@1.0.30'
 export { get } from 'https://esm.sh/radash@11.0.0'
 
 // @headlessui/react:
-export { } from 'https://esm.sh/@headlessui/react@1.7.14?alias=react:preact/compat,react-dom:preact/compat,@types/react:preact/compat&external=preact/compat'
+export {} from 'https://esm.sh/@headlessui/react@1.7.14?alias=react:preact/compat,react-dom:preact/compat,@types/react:preact/compat&external=preact/compat'
 
 // shiki-es:
 export {
