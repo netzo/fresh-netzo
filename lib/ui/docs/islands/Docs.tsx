02cfd5c802975f2b1d3b92fe0aa9600ac860c7cc
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-20 17:27:47 +0200

    0.2.6

--- lib/ui/docs/islands/Docs.tsx
+++ lib/ui/docs/islands/Docs.tsx
@@ -23,7 +23,7 @@ import {
 } from "netzo/ui/components/mod.ts";
 import ShowSource from "./ShowSource.tsx";
 
-const VERSION = "0.2.5";
+const VERSION = "0.2.6";
 const REPO = "https://github.com/netzo/netzo/tree/main/lib/ui";
 
 export default function Demo() {
