85fb7a36f7ce1fdb2a354b93774b0ac457177409
Arturo Romero <arturo.ro.ka@gmail.com>
2023-07-25 17:51:38 +0200

    feat(apis): add mailgun, fix sendgrid

---
+++ lib/apis/mailgun/mod.ts
@@ -0,0 +1,32 @@
+import { createApi } from "../_create-api/mod.ts";
+import { auth } from "../_create-api/auth/mod.ts";
+
+/**
+ * SDK constructor function for the Mailgun API
+ *
+ * @see https://netzo.io/docs/netzo/apis/mailgun
+ *
+ * @param {string} apiKey - the API key to use for authentication
+ * @returns {object} - an object of multiple utilities for the API
+ */
+export const mailgun = ({
+  apiKey = Deno.env.get("MAILGUN_API_KEY")!,
+  domainName = Deno.env.get("MAILGUN_DOMAIN_NAME")!,
+}) => {
+  const api = createApi({
+    baseURL: `https://api.mailgun.net/v3/${domainName}`,
+    headers: {
+      "content-type": "application/json",
+    },
+    async onRequest(ctx) {
+      await auth({
+        type: "apiKey",
+        in: "header",
+        name: "api",
+        value: `${apiKey}`,
+      }, ctx);
+    },
+  });
+
+  return { api };
+};
