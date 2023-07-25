53ec1f863ad2a29fff43f3abb72325fb40b0ef0b
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-25 17:36:41 +0200

    feat(apis): add exported `GooglesheetOptions` interface for `googlesheets` API

--- lib/apis/googlesheets/mod.ts
+++ lib/apis/googlesheets/mod.ts
@@ -1,25 +1,29 @@
 import { createApi } from "../_create-api/mod.ts";
 import { auth } from "../_create-api/auth/mod.ts";
 
+export interface GooglesheetOptions {
+  googleServiceAccountCredentials: string;
+  scope?: Array<"drive" | "drive.readonly" | "drive.file" | "spreadsheets" | "spreadsheets.readonly">;
+  spreadsheetId: string;
+}
+
 /**
  * SDK constructor function for the Google Sheets API
  *
  * @see https://netzo.io/docs/netzo/apis/googlesheets
  *
+ * @param {string} googleServiceAccountCredentials - the Google Service Account Credentials to use for authentication
+ * @param {string} scope - the scope to use for authentication (default: ['spreadsheets'])
  * @param {string} spreadsheetId - the spreadsheet ID to construct the base URL
- * @param {string} clientId - the client ID to use for authentication
- * @param {string} clientSecret - the client secret to use for authentication
  * @returns {object} - an object of multiple utilities for the API
  */
 export const googlesheets = ({
   googleServiceAccountCredentials = Deno.env.get(
     "GOOGLE_SERVICE_ACCOUNT_CREDENTIALS",
   )!,
-  googleAuthOptions = {
-    scope: ["https://www.googleapis.com/auth/spreadsheets"],
-  },
+  scope = ['spreadsheets'],
   spreadsheetId = Deno.env.get("GOOGLESHEETS_SPREADSHEET_ID")!,
-}) => {
+}: GooglesheetOptions) => {
   const api = createApi({
     baseURL: `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}`,
     headers: {
@@ -30,7 +34,9 @@ export const googlesheets = ({
       await auth({
         type: "googlejwtsa",
         googleServiceAccountCredentials,
-        googleAuthOptions,
+        googleAuthOptions: {
+          scope: scope.map(s => `https://www.googleapis.com/auth/${s}`),
+        },
       }, ctx);
     },
   });
