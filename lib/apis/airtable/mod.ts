8ee2c5a6da9d4c826394b0f811ff85bdabfdb4fd
Arturo Romero <arturo.ro.ka@gmail.com>
2023-07-25 14:16:41 +0200

    chore: edit airtable api client

--- lib/apis/airtable/mod.ts
+++ lib/apis/airtable/mod.ts
@@ -6,17 +6,14 @@ import { auth } from "../_create-api/auth/mod.ts";
  *
  * @see https://netzo.io/docs/netzo/apis/airtable
  *
- * @param {string} accountName - the account name to construct the base URL
  * @param {string} personalAccessToken - the API key to use for authentication
  * @returns {object} - an object of multiple utilities for the API
  */
 export const airtable = ({
-  baseId = Deno.env.get('AIRTABLE_BASE_ID'),
-  tableIdOrName = Deno.env.get('AIRTABLE_TABLE_ID_OR_NAME'),
   personalAccessToken = Deno.env.get('AIRTABLE_PERSONAL_ACCESS_TOKEN'),
 }) => {
   const api = createApi({
-    baseURL: `https://api.airtable.com/v0/${baseId}YOUR_BASE_ID/${tableIdOrName}`,
+    baseURL: `https://api.airtable.com/v0`,
     headers: {
       'content-type': 'application/json',
     },
