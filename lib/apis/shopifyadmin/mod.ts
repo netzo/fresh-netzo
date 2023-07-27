052d5c40e3232803158023678c63baed6402171c
kbastamow <bastamowkat@gmail.com>
2023-07-27 15:15:49 +0300

    wip(apis): Shopify - functions and types

--- lib/apis/shopifyadmin/mod.ts
+++ lib/apis/shopifyadmin/mod.ts
@@ -1,5 +1,20 @@
 import { createApi } from "../_create-api/mod.ts";
 import { auth } from "../_create-api/auth/mod.ts";
+import {
+  Customer,
+  CustomerSpecific,
+  Order,
+  OrderSpecific,
+  OrderStatus,
+  Payout,
+  Product,
+  ProductSpecific,
+  AddOrUpdateCustomer,
+  QueryCustomers,
+  QueryOrder,
+  QueryPayouts,
+  QueryProducts,
+} from "@/lib/apis/shopifyadmin/types.ts";
 
 export interface ShopifyAdminOptions {
   storeName: string;
@@ -37,5 +52,119 @@ export const shopifyadmin = ({
     },
   });
 
-  return { api };
+  /**
+   * Get customers from Shopify
+   */
+  const getCustomers = async (
+    query: QueryCustomers = {},
+  ): Promise<Customer[]> => {
+    const result = await api.customers.get(query);
+    return result.customers;
+  };
+
+  /**
+   * Get a single customer from Shopify
+   */
+  const getSingleCustomer = async (
+    customerId: string,
+    fields = "",
+  ): Promise<CustomerSpecific> => {
+    const result = await api.customers[`${customerId}`].get(fields);
+    return result.customer;
+  };
+
+  /**
+   * Get orders belonging to a specific customer from Shopify
+   */
+
+  //must check if order object the same
+  const getCustomerOrders = async (
+    customerId: string,
+    status: OrderStatus = "any",
+  ): Promise<Order[]> => {
+    const result = await api.customers[`${customerId}`].orders.get(status);
+    return result.orders;
+  };
+
+  /**
+   * Add a new customer in Shopify
+   */
+  const addCustomer = async (
+    data: AddOrUpdateCustomer = {},
+  ): Promise<CustomerSpecific> => {
+    const result = await api.customers.post(data);
+    return result.customer;
+  };
+
+  /**
+   * Update customer details in Shopify
+   */
+  const updateCustomer = async (
+    customerId: string,
+    data: AddOrUpdateCustomer = {},
+  ): Promise<CustomerSpecific> => {
+    const result = await api.customers[`${customerId}`].put(data);
+    return result.customer;
+  };
+
+  /**
+   * Get orders from Shopify
+   */
+
+  const getOrders = async (query: QueryOrder = {}): Promise<Order[]> => {
+    const result = await api.orders.get(query);
+    return result.orders;
+  };
+
+  /**
+   * Get a single order from Shopify
+   */
+  const getSingleOrder = async (
+    orderId: string,
+    fields = "",
+  ): Promise<OrderSpecific> => {
+    const result = await api.orders[`${orderId}`].get(fields);
+    return result.order;
+  };
+
+  /**
+   * Get products from Shopify
+   */
+  const getProducts = async (query: QueryProducts = {}): Promise<Product[]> => {
+    const result = await api.products.get(query);
+    return result.products;
+  };
+
+  /**
+   * Get a single product from Shopify
+   */
+  const getSingleProduct = async (
+    productId: string,
+    fields = "",
+  ): Promise<ProductSpecific> => {
+    const result = await api.products[`${productId}`].get(fields);
+    return result.product;
+  };
+
+  /**
+   * Get payouts from Shopify
+   */
+  const getPayouts = async (query: QueryPayouts = {}): Promise<Payout[]> => {
+    const result = await api.shopify_payments.payouts.get(query);
+    return result.payouts;
+  };
+
+  return {
+    api,
+    getCustomers,
+    getSingleCustomer,
+    getCustomerOrders,
+    addCustomer,
+    updateCustomer,
+    getOrders,
+    getSingleOrder,
+    getProducts,
+    getSingleProduct,
+    getPayouts,
+  };
 };
