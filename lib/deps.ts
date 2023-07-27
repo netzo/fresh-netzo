b4efda8b102db50ec4afbfa55995f15f198667cb
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-27 12:56:38 +0200

    chore(deps): bump fresh to `1.3.1`

--- lib/deps.ts
+++ lib/deps.ts
@@ -11,7 +11,7 @@ export {
 export type {
   MiddlewareHandler,
   Plugin,
-} from "https://deno.land/x/fresh@1.3.0/server.ts";
+} from "https://deno.land/x/fresh@1.3.1/server.ts";
 
 // preact:
 export {
