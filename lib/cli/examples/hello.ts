bf6fa7945a6d724a16688d78942402320dee2416
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-07-27 15:24:08 +0200

    chore(cli): remove examples directory

--- lib/cli/examples/hello.ts
+++
@@ -1,9 +0,0 @@
-async function handler(_req: Request) {
-  const text = await Deno.readTextFile(new URL(import.meta.url));
-  return new Response(text, {
-    headers: { "content-type": "text/plain; charset=utf8" },
-  });
-}
-
-console.log("Listening on http://localhost:8000");
-Deno.serve(handler);
