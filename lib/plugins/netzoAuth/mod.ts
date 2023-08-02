f8b0db2c27bf633d4c4f30b9932414ca3b6ee6c5
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-02 18:11:12 +0200

    fix(plugins): add `visibility: 'protected'` to `netzoAuth`

--- lib/plugins/netzoAuth/mod.ts
+++ lib/plugins/netzoAuth/mod.ts
@@ -1,13 +1,17 @@
 import type { MiddlewareHandler, Plugin } from "../deps.ts";
 
-export interface NetzoAuthOptions {
-  visibility: "private" | "protected" | "public";
-  tokens?: string[];
+export type NetzoAuthOptions = {
+  visibility: "private";
+} | {
+  visibility: "protected";
+  tokens: string[];
+} | {
+  visibility: "public";
 }
 
 /*
 The following lists the possible header combinations when
-making a request to a project/deployment from the sources:
+making a request to a project/deployment from any source:
 
 [app] allow all requests coming from app.netzo.io
   [app:iframe] allow embedding as <iframe src /> in app (e.g. in /projects/:uid page)
@@ -30,6 +34,7 @@ making a request to a project/deployment from the sources:
 const createHandler = (options: NetzoAuthOptions): MiddlewareHandler => {
   return async (req, ctx) => {
     if (["internal"].includes(ctx.destination)) return await ctx.next();
+    // else if "internal" | "static" | "notFound":
 
     const url = new URL(req.url);
     const token = req.headers.get("x-token") ?? url.searchParams.get("token");
@@ -39,27 +44,32 @@ const createHandler = (options: NetzoAuthOptions): MiddlewareHandler => {
     const referer = req.headers.get("referer"); // SOMETIMES SET e.g. https://app.netzo.io/some-path
 
     // simple heuristics to determine source of request:
-    const sources = {
-      app: referer === "https://app.netzo.io/",
-      external: !origin && !referer,
+    const isNetzoApp = (url: string) => !!url && new URL(url).origin === "https://app.netzo.io";
+    const isNetzo = (url: string) => !!url && (new URL(url).host).endsWith('netzo.io');
+    const is = {
+      app: isNetzoApp(referer!),
+      external: !isNetzo(origin!) || !isNetzo(referer!),
     };
 
-    console.log({ ...options, origin, referer, sources });
+    console.log({ destination: ctx.destination, options, origin, referer, is });
 
     switch (options.visibility) {
       case "private": {
         // [external] deny all
-        if (sources.external) {
+        if (is.external) {
           throw new Error("Private deployments cannot be accessed externally");
-        } // [app] allow all
+        }
+        // [app] allow all
         else return await ctx.next();
       }
       case "protected": {
+        if (!options.tokens?.length) {
+          throw new Error("Missing required option 'tokens' in netzoAuth plugin");
+        }
         // [external] allow if API key is provided and valid
-        if (sources.external && token && !options.tokens?.includes(token)) {
+        if (is.external && !options.tokens.includes(token!)) {
           throw new Error("Protected deployments require a valid token");
         }
-
         // [app] allow all
         return await ctx.next();
       }
