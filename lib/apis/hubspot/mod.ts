d0bcda0e53c6197128a7208a107b9234831520a9
kbastamow <bastamowkat@gmail.com>
2023-08-08 13:58:21 +0300

    feat(apis): Hubspot tests base

--- lib/apis/hubspot/mod.ts
+++ lib/apis/hubspot/mod.ts
@@ -1,14 +1,14 @@
 import { createApi } from "../_create-api/mod.ts";
 import { auth } from "../_create-api/auth/mod.ts";
 export type {
-  AddContactResponse,
   AddDealResponse,
+  AddOrUpdateContactResponse,
   Contacts,
   Deals,
   Form,
   FormSubmissions,
-  QueryAddContact,
   QueryAddDeal,
+  QueryAddOrUpdateContact,
   QueryContacts,
   QueryDeals,
   QueryForms,
