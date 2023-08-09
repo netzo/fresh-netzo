20e6ad868b00749e7601b6ccfcddd07b4037c5ab
kbastamow <bastamowkat@gmail.com>
2023-08-09 14:40:52 +0300

    feat(apis): Googlesheet interfaces and tests

--- lib/apis/googlesheets/mod.test.ts
+++ lib/apis/googlesheets/mod.test.ts
@@ -1,11 +1,13 @@
-import { assertExists } from "../deps.ts";
+import { assertExists, assertEquals } from "../deps.ts";
 import { googlesheets } from "./mod.ts";
 
 Deno.test("googlesheets", async (t) => {
   const { options } = await import("./googlesheets.options.ts");
-  const { api } = googlesheets(options);
+  const { api, resultToRows } = googlesheets(options);
 
   const range = "Sheet1!A:D";
+  const range_to_update = "Sheet1!A32:D32"
+  const range_to_delete = range_to_update
 
   await t.step("api", async () => {
     const result = await api.values[range].get();
@@ -13,4 +15,47 @@ Deno.test("googlesheets", async (t) => {
     assertExists(result.majorDimension);
     assertExists(result.values);
   });
+
+  await t.step("get rows", async () => {
+    const result = await api.values[range].get()
+    console.log(result.values)
+    assertExists(result.values);
+    assertExists(resultToRows(result))
+    console.log(resultToRows(result))
+    assertEquals(Array.isArray(resultToRows(result)), true);
+  });
+
+  await t.step("get row", async () => {
+    const result = await api.values[range].get()
+    console.log(result.values)
+    assertExists(result.values);
+    assertExists(resultToRows(result))
+    console.log(resultToRows(result))
+    assertEquals(Array.isArray(resultToRows(result)), true);
+  })
+
+  await t.step("get row", async () => {
+    const result = await api.values[range].get()
+    assertExists(result.values[1]);
+    assertExists(resultToRows(result)[0]);
+    assertEquals(Array.isArray(resultToRows(result)[0]), true);
+  })
+
+
+  //To be completed
+  // await t.step("add row", async () => {
+
+  // })
+
+  await t.step("update row", async () => {
+    const result = await api.values[range_to_update].put({ values: ["updatedValue1", "updatedValue2, updatedValue3, updatedValue4"] })
+    assertExists(result.updatedData)
+    assertEquals(typeof result.updatedData, "object")
+  })
+
+  await t.step("delete row", async () => {
+    const data = await api.values[range_to_delete].put({ values: ["updatedValue1", "updatedValue2, updatedValue3, updatedValue4"] })
+    assertExists(data)
+    assertEquals(typeof data, "object")
+  })
 });
--- lib/apis/googlesheets/mod.ts
+++ lib/apis/googlesheets/mod.ts
@@ -1,6 +1,12 @@
 import { createApi } from "../_create-api/mod.ts";
 import { auth } from "../_create-api/auth/mod.ts";
-
+export type {
+  ValueRange,
+  RequestAddOrUpdateValues,
+  UpdateValuesResponse,
+  AddValuesResponse,
+  DeleteValuesResponse
+} from "./types.ts"
 export interface GoogleSheetsOptions {
   googleServiceAccountCredentials: string;
   scope?: Array<
@@ -57,62 +63,5 @@ export const googlesheets = ({
       ), {});
   };
 
-  /**
-   * Get spreadheet rows from a given range (first row treated as header)
-   * @example const rows = await getRows<{ name: string }>('A1:A10');
-   * @param {string} range - the range to get rows from
-   * @returns {Promise<T[]>} - an array of rows
-   */
-  const getRows = async <T>(range: string): Promise<T[]> => {
-    const result = await api.values[range].get();
-    return resultToRows(result);
-  };
-
-  /**
-   * Get spreadheet row from a given range (first row treated as header)
-   * @example const row = await getRow<{ name: string }>('A1:A1');
-   * @param {string} range - the range to get row from
-   * @returns {Promise<T>} - a row
-   */
-  const getRow = async <T>(range: string): Promise<T> => {
-    const result = await api.values[range].get();
-    return resultToRows(result)[0];
-  };
-
-  /**
-   * Add rows to a given range (first row treated as header)
-   * @example const rows = await addRows<{ name: string }>('A1:A10', [{ name: 'John' }]);
-   * @param {string} range - the range to add rows to
-   * @param {T[]} rows - an array of rows to add
-   * @returns {Promise<T[]>} - an array of rows
-   */
-  const addRows = async <T>(range: string, rows: T[]): Promise<T[]> => {
-    const result = await api.values[range].post({ values: rows });
-    return resultToRows(result);
-  };
-
-  /**
-   * Update row in a given range (first row treated as header)
-   * @example const row = await updateRow<{ name: string }>('A1:A1', { name: 'John' });
-   * @param {string} range - the range to update row in
-   * @param {T} row - a row to update
-   * @returns {Promise<T>} - a row
-   */
-  const updateRow = async <T>(range: string, row: T): Promise<T> => {
-    const result = await api.values[range].put({ values: [row] });
-    return resultToRows(result)[0];
-  };
-
-  /**
-   * Delete row in a given range (first row treated as header)
-   * @example const row = await deleteRow<{ name: string }>('A1:A1');
-   * @param {string} range - the range to delete row in
-   * @returns {Promise<T>} - a row
-   */
-  const deleteRow = async <T>(range: string): Promise<T> => {
-    const result = await api.values[range].delete();
-    return resultToRows(result)[0];
-  };
-
-  return { api, getRows, getRow, addRows, updateRow, deleteRow };
+  return { api, resultToRows };
 };
