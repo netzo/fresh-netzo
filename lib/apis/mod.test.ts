b1fdb63cbfc39a86490956cdbf7f84e1c12fde63
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-10 11:44:02 +0200

    chore(ui): cleanup code

---
+++ lib/apis/mod.test.ts
@@ -0,0 +1,31 @@
+import { assertExists } from "./deps.ts";
+import * as apis from "./mod.ts";
+
+Deno.test("apis", async (t) => {
+  await t.step("declarations", () => {
+    assertExists(apis.activecampaign);
+    assertExists(apis.clarifai);
+    assertExists(apis.cloudflare);
+    assertExists(apis.discord);
+    assertExists(apis.ergastf1);
+    assertExists(apis.facturama);
+    assertExists(apis.fathomanalytics);
+    assertExists(apis.faunadb);
+    assertExists(apis.github);
+    assertExists(apis.googleappsheet);
+    assertExists(apis.googlesheets);
+    assertExists(apis.jsonplaceholder);
+    assertExists(apis.monday);
+    assertExists(apis.mongodbatlasdata);
+    assertExists(apis.netzo);
+    assertExists(apis.openai);
+    assertExists(apis.pandadoc);
+    assertExists(apis.plain);
+    assertExists(apis.purpleair);
+    assertExists(apis.restdb);
+    assertExists(apis.sendgrid);
+    assertExists(apis.brevo);
+    assertExists(apis.spacexland);
+    assertExists(apis.whatsappbusiness);
+  });
+});
