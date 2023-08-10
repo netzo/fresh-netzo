a0d2a65910abaacb01f84844d370c0f98fd17d12
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-10 10:12:13 +0200

    fix(plugins): theme for unocss.shadcn preset

--- lib/ui/unocss.shadcn.ts
+++ lib/ui/unocss.shadcn.ts
@@ -56,7 +56,15 @@ export function presetShadcn(options: PresetShadcnOptions = {}): Preset<Theme> {
     ],
     preflights: [
       {
-        // generated at https://ui.shadcn.com/themes
+        getCSS: () => `
+          @keyframes shadcn-down { from{ height: 0 } to { height: var(--radix-accordion-content-height)} }
+          @keyframes shadcn-up { from{ height: var(--radix-accordion-content-height)} to { height: 0 } }
+          @keyframes shadcn-enter { from{ opacity: var(--un-enter-opacity, 1); transform: translate3d(var(--un-enter-translate-x, 0), var(--un-enter-translate-y, 0), 0) scale3d(var(--un-enter-scale, 1), var(--un-enter-scale, 1), var(--un-enter-scale, 1)) rotate(var(--un-enter-rotate, 0)) } }
+          @keyframes shadcn-exit { to{ opacity: var(--un-exit-opacity, 1); transform: translate3d(var(--un-exit-translate-x, 0), var(--un-exit-translate-y, 0), 0) scale3d(var(--un-exit-scale, 1), var(--un-exit-scale, 1), var(--un-exit-scale, 1)) rotate(var(--un-exit-rotate, 0)) } }
+        `,
+      },
+      {
+        // generated from https://ui.shadcn.com/themes
         getCSS: () => `
         @layer base {
           :root {
@@ -106,7 +114,115 @@ export function presetShadcn(options: PresetShadcnOptions = {}): Preset<Theme> {
         }`,
       },
     ],
-    rules: [],
+    variants: [variantGroupDataAttribute.match],
+    rules: [
+      [
+        "accordion-down",
+        {
+          animation: "shadcn-down 0.2s ease-out",
+        },
+      ],
+      [
+        "accordion-up",
+        {
+          animation: "shadcn-up 0.2s ease-out",
+        },
+      ],
+      [
+        "animate-in",
+        {
+          "animation-name": "shadcn-enter",
+          "animation-duration": "var(--un-animate-duration)",
+          "--un-animate-duration": "150ms",
+          "--un-enter-opacity": "initial",
+          "--un-enter-scale": "initial",
+          "--un-enter-rotate": "initial",
+          "--un-enter-translate-x": "initial",
+          "--un-enter-translate-y": "initial",
+        },
+      ],
+      [
+        "animate-out",
+        {
+          "animation-name": "shadcn-exit",
+          "animation-duration": "var(--un-animate-duration)",
+          "--un-animate-duration": "150ms",
+          "--un-exit-opacity": "initial",
+          "--un-exit-scale": "initial",
+          "--un-exit-rotate": "initial",
+          "--un-exit-translate-x": "initial",
+          "--un-exit-translate-y": "initial",
+        },
+      ],
+      [
+        /^fade-in-?(.+)?$/,
+        ([, d]) => ({
+          "--un-enter-opacity": `${Number(handleMatchNumber(d) || 0) / 100}`,
+        }),
+      ],
+      [
+        /^fade-out-?(.+)?$/,
+        ([, d]) => ({
+          "--un-exit-opacity": `${Number(handleMatchNumber(d) || 0) / 100}`,
+        }),
+      ],
+      [
+        /^zoom-in-?(.+)?$/,
+        ([, d]) => ({
+          "--un-enter-scale": `${Number(handleMatchNumber(d) || 0) / 100}`,
+        }),
+      ],
+      [
+        /^zoom-out-?(.+)?$/,
+        ([, d]) => ({
+          "--un-out-scale": `${Number(handleMatchNumber(d) || 0) / 100}`,
+        }),
+      ],
+      [
+        /^spin-in-?(.+)?$/,
+        ([, d]) => ({
+          "--un-enter-rotate": `${Number(handleMatchNumber(d) || 0)}deg`,
+        }),
+      ],
+      [
+        /^spin-out-?(.+)?$/,
+        ([, d]) => ({
+          "--un-exit-rotate": `${Number(handleMatchNumber(d) || 0)}deg`,
+        }),
+      ],
+      [
+        /^slide-in-from-top-?(.+)?$/,
+        ([, d]) => ({ "--un-enter-translate-y": `-${handleMatchRem(d)}` }),
+      ],
+      [
+        /^slide-in-from-bottom-?(.+)?$/,
+        ([, d]) => ({ "--un-enter-translate-y": handleMatchRem(d) }),
+      ],
+      [
+        /^slide-in-from-left-?(.+)?$/,
+        ([, d]) => ({ "--un-enter-translate-x": `-${handleMatchRem(d)}` }),
+      ],
+      [
+        /^slide-in-from-right-?(.+)?$/,
+        ([, d]) => ({ "--un-enter-translate-x": handleMatchRem(d) }),
+      ],
+      [
+        /^slide-out-to-top-?(.+)?$/,
+        ([, d]) => ({ "--un-exit-translate-y": `-${handleMatchRem(d)}` }),
+      ],
+      [
+        /^slide-out-to-bottom-?(.+)?$/,
+        ([, d]) => ({ "--un-exit-translate-y": handleMatchRem(d) }),
+      ],
+      [
+        /^slide-out-to-left-?(.+)?$/,
+        ([, d]) => ({ "--un-exit-translate-x": `-${handleMatchRem(d)}` }),
+      ],
+      [
+        /^slide-out-to-right-?(.+)?$/,
+        ([, d]) => ({ "--un-exit-translate-x": handleMatchRem(d) }),
+      ],
+    ],
     theme: {
       colors: {
         border: "hsl(var(--border))",
@@ -143,10 +259,8 @@ export function presetShadcn(options: PresetShadcnOptions = {}): Preset<Theme> {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
+      fontFamily: {
+        sans: "Avenir, Helvetica, Arial, sans-serif",
       },
     },
   };
