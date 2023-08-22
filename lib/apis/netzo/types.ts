02c2b7cef099697b1d172eed940e6a9ec43168a2
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-22 13:02:08 +0200

    feat(apis): add types to `netzo` from `@netzo/apis`

---
+++ lib/apis/netzo/types.ts
@@ -0,0 +1,13 @@
+export type {
+  ApiKey,
+  AuditLog,
+  Deployment,
+  Log,
+  Project,
+  Request,
+  Template,
+  User,
+  Variable,
+  Workspace,
+  WorkspaceUser,
+} from "https://esm.sh/v131/@netzo/api@1.0.31/lib/client.d.ts";
