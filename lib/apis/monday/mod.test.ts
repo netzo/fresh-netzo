b88797815183de1f46213b964dc8027e7cfc5f4c
kbastamow <bastamowkat@gmail.com>
2023-08-22 16:31:22 +0300

    feat(apis): tests monday

---
+++ lib/apis/monday/mod.test.ts
@@ -0,0 +1,37 @@
+import "https://deno.land/std@0.198.0/dotenv/load.ts";
+import { assertEquals, assertExists } from "../deps.ts";
+import { monday } from "./mod.ts";
+
+Deno.test("monday", async (t) => {
+  const { api } = monday({
+    apiKey: Deno.env.get("MONDAY_API_KEY")!,
+  });
+
+  await t.step("find boards", async () => {
+    const query = `
+    query { 
+        boards (limit:5) { 
+            name 
+            id 
+        }
+    }`;
+    const data = await api.post(query);
+    assertExists(data);
+    assertEquals(typeof data, "object");
+  });
+
+  //   await t.step("get board", async () => {
+  //     const query = `
+  //     query {
+  //         boards (ids: ${"BOARD_ID"}) {
+  //             name
+  //             state
+  //             id
+  //             permissions
+  //         }
+  //     }`;
+  // const data = await api.post(query)
+  //     assertExists(data);
+  //     assertEquals(typeof data, "object");
+  //   });
+});
