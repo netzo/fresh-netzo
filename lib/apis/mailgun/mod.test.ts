d5616fa89815bb83d249219e5f935dce3783b6b5
kbastamow <bastamowkat@gmail.com>
2023-08-22 15:22:54 +0300

    feat(apis): test  mailgun

---
+++ lib/apis/mailgun/mod.test.ts
@@ -0,0 +1,15 @@
+import "https://deno.land/std@0.198.0/dotenv/load.ts";
+import { assertEquals, assertExists } from "../deps.ts";
+import { mailgun } from "./mod.ts";
+
+Deno.test("mailgun", async (t) => {
+  const { api } = mailgun({
+    apiKey: Deno.env.get("MAILGUN_API_KEY")!,
+  });
+
+  await t.step("find mailing lists", async () => {
+    const result = await api.v3.lists.pages.get();
+    assertExists(result.items);
+    assertEquals(Array.isArray(result.items), true);
+  });
+});
