453294557bf006cfaaa3fd14bf6e998d224d1e97
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-29 19:21:36 +0200

    fix(plugins): set `crossOrigin` as default script attribute for `htmx` plugin

--- lib/ui/plugins/htmx/main.ts
+++ lib/ui/plugins/htmx/main.ts
@@ -5,6 +5,6 @@ export default function main() {
     src: "https://unpkg.com/htmx.org@1.9.5",
     integrity:
       "sha384-xcuj3WpfgjlKF+FXhSQFQ0ZNr39ln+hwjN3npfM9VBnUskLolQAcN80McRIVOPuO",
-    crossorigin: "anonymous",
+    crossOrigin: "anonymous", // must be camelCase (see https://stackoverflow.com/a/48194442)
   });
 }
