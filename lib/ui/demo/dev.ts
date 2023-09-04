1f323233a3757d10503b7d3e781d050e3fc14952
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-09-04 02:19:20 +0200

    refactor(fresh): move `import "std/dotenv/load.ts"` from `main.ts` to `dev.ts`

--- lib/ui/demo/dev.ts
+++ lib/ui/demo/dev.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
+import "std/dotenv/load.ts";
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
 
--- www/src/docs/netzo/ui/plugins/src/main.ts
+++ www/src/docs/netzo/ui/plugins/src/main.ts
@@ -4,8 +4,6 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import 'std/dotenv/load.ts'
-
 import manifest from './fresh.gen.ts'
 import config from './fresh.config.ts'
 import { start } from '$fresh/server.ts'
