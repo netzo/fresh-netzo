b0c109ac385c4c62bb758071451680ff1612b78d
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-09-19 23:45:19 +0200

    refactor(config): replace `fresh.config` with `netzo.config`

--- templates/app-sales-dashboard/src/main.ts
+++ templates/app-sales-dashboard/src/main.ts
@@ -7,6 +7,6 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
+import config from "./netzo.config.ts";
 
 await start(manifest, config);
