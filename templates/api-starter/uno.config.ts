46c75cf480dcbdc9ef38a56b992a21b5d0559177
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-10-18 22:13:20 +0200

    chore(api-starter): remove api-starter (for now)

--- templates/api-starter/uno.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "netzo/plugins/unocss/mod.ts";
-import { presetNetzo } from "netzo/plugins/unocss/preset-netzo.ts";
-
-export default defineConfig({
-  selfURL: import.meta.url,
-  presets: [presetNetzo()],
-});
