5d05e881fd3c12a6a952dd257187e40526a71df2
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-10-20 14:35:47 +0200

    fix(templates): update broken import

--- templates/app-starter/netzo.config.ts
+++ templates/app-starter/netzo.config.ts
@@ -1,19 +1,14 @@
 import "std/dotenv/load.ts";
 import { defineNetzoConfig } from "netzo/config.ts";
 import { createGitHubOAuthConfig } from "deno_kv_oauth/mod.ts";
-import { authenticationPlugin } from "../../lib/authentication/fresh.ts";
-import appLayout from "netzo/plugins/appLayout/mod.ts";
+import { authenticationPlugin } from "netzo/authentication/fresh.ts";
 import errorPages from "netzo/plugins/errorPages/mod.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
 export default defineNetzoConfig({
-  project: "sheer-marlin-436696",
-  entrypoint: "main.ts",
+  project: "PROJECT_UID",
   visibility: { level: "public" },
-  modules: {
-    authentication: {},
-  },
   plugins: [
     errorPages(),
     authenticationPlugin(createGitHubOAuthConfig()),
