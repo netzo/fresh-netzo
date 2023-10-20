7f2da55150ff40d8ddc0abfe88328628435557b5
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-10-20 14:50:02 +0200

    chore(templates): update env vars

--- templates/app-starter/netzo.config.ts
+++ templates/app-starter/netzo.config.ts
@@ -6,16 +6,16 @@ import errorPages from "netzo/plugins/errorPages/mod.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
+// replace these with your oauth credentials:
+Deno.env.set("GITHUB_CLIENT_ID", "a05e7d546dc9ff966421")
+Deno.env.set("GITHUB_CLIENT_SECRET", "8af9e7ea7ea196b9371b98a4d93b745fc677ffeb")
+
 export default defineNetzoConfig({
   project: "PROJECT_UID",
   visibility: { level: "public" },
   plugins: [
     errorPages(),
-    // replace these with your oauth credentials:
-    authenticationPlugin(createGitHubOAuthConfig({
-      clientId: "a05e7d546dc9ff966421",
-      clientSecret: "8af9e7ea7ea196b9371b98a4d93b745fc677ffeb"
-    })),
+    authenticationPlugin(createGitHubOAuthConfig()),
     twindPlugin(twindConfig),
   ],
 });
