4cee341a521df1287d375de8a4b432edc26dc655
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-11-02 15:31:55 +0100

    fix(templates): remove hard-coded project from netzo.config

--- templates/minimal/netzo.config.ts
+++ templates/minimal/netzo.config.ts
@@ -1,6 +1,4 @@
 import "std/dotenv/load.ts";
 import { defineNetzoConfig } from "netzo/config/mod.ts";
 
-export default defineNetzoConfig({
-  project: "rare-ptarmigan-142048"
-});
+export default defineNetzoConfig({});
