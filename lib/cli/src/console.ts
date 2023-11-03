a53690a41efc55515714f2a0406cc4aaf64c16f8
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-11-03 23:17:18 +0100

    feat(cli): enhance log styles

--- lib/cli/src/console.ts
+++ lib/cli/src/console.ts
@@ -1,29 +1,34 @@
-import { blue, bold, green, red, yellow } from "../deps.ts";
+import { blue, bold, green, red, white, yellow } from "../deps.ts";
 
 export const LOGS = {
   missingApiKey:
-    "Missing API key. Set via --api-key flag or NETZO_API_KEY environment variable to avoid passing it each time.",
+    "Missing API key. Set via --api-key flag or NETZO_API_KEY environment variable.",
+  skippingLoadingOfEnvVars: `Skipping loading of "development" environment variables.`,
   notFoundProject:
     "Project not found. Check the project UID and make sure your API key has access to the project.",
 } as const;
 
-export function printSuccess(message: string) {
-  console.log(green(`${bold("success")}: ${message}`));
+export function log(message: string) {
+  console.log(white(`${bold("[netzo]")} ${message}`));
 }
 
-export function printInfo(message: string) {
-  console.info(blue(`${bold("info")}: ${message}`));
+export function logInfo(message: string) {
+  console.info(blue(`${bold("[netzo]")} ${message}`));
 }
 
-export function printWarning(message: string) {
-  console.warn(yellow(`${bold("warning")}: ${message}`));
+export function logSuccess(message: string) {
+  console.log(green(`${bold("[netzo]")} ${message}`));
 }
 
-export function printError(message: string) {
-  console.error(red(`${bold("error")}: ${message}`));
+export function logWarning(message: string) {
+  console.warn(yellow(`${bold("[netzo]")} ${message}`));
+}
+
+export function logError(message: string) {
+  console.error(red(`${bold("[netzo]")} ${message}`));
 }
 
 export function error(message: string): never {
-  printError(message);
+  logError(message);
   Deno.exit(1);
 }
