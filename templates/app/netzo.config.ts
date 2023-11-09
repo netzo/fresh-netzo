d9dc6353305dedd0fb1630a6df5b4d9823422d3b
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-11-09 07:09:01 -0600

    chore(deps): bump fresh to 1.5.4

--- templates/app/netzo.config.ts
+++ templates/app/netzo.config.ts
@@ -1,6 +1,5 @@
 import { defineNetzoConfig } from "netzo/config/mod.ts";
 import { auth } from "netzo/plugins/auth/mod.ts";
-import { appLayout } from "netzo/plugins/appLayout/mod.ts";
 import { api } from "netzo/plugins/api/mod.ts";
 import { createGitHubOAuthConfig } from "deno_kv_oauth/mod.ts";
 import { errorPages } from "netzo/plugins/errorPages/mod.ts";
@@ -13,17 +12,6 @@ export default defineNetzoConfig({
     auth({
       email: {},
       oauth2: createGitHubOAuthConfig(),
-    }),
-    appLayout({
-      title: "Agbar | Admin Panel",
-      description: "Herramienta de administración y gestion de Agbar",
-      favicon:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi-vXRSWKKnrGxki2DRNUfFrB4W_SUPoGAoPVJx7BCJoEgxUosgHczuX6C7PX3gaCk0RQ&usqp=CAU",
-      image: {
-        src:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi-vXRSWKKnrGxki2DRNUfFrB4W_SUPoGAoPVJx7BCJoEgxUosgHczuX6C7PX3gaCk0RQ&usqp=CAU",
-        // class: "dark:filter-invert",
-      },
     }),
     api(),
     errorPages(),
