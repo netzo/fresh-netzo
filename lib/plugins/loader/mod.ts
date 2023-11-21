7dff4d29d19f1234ccd8476742f7e5fd8f8095ec
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-11-21 13:49:34 +0100

    feat(plugin): add loader plugin

---
+++ lib/plugins/loader/mod.ts
@@ -0,0 +1,71 @@
+import { Plugin } from "$fresh/server.ts";
+import type { NetzoState } from "netzo/config/mod.ts";
+
+export type LoaderOptions = {
+  color?: string;
+  height?: string;
+};
+
+export type LoaderState = {};
+
+const ANIMATION = "loader 12s ease infinite";
+
+export const loader = ({
+  color = "linear-gradient(to right, #000000 0%, #434343 51%, #000000 100%)",
+  height = "4px",
+} = {}): Plugin<NetzoState> => {
+  const main = `data:application/javascript,
+  export default function(_) {
+    const loader = document.getElementById("__loader");
+    const rules = loader.sheet.rules[0];
+    window.showLoader = () => {
+      rules.style.display = "block";
+      rules.style.animation = "${ANIMATION}";
+    };
+    window.hideLoader = () => {
+      rules.style.animationDuration = ".3s";
+      setTimeout(() => {
+        rules.style.display = "none";
+        rules.style.animation = "none";
+      }, 300);
+    };
+    window.addEventListener('load', hideLoader);
+  }
+  `;
+  return {
+    name: "loader",
+    entrypoints: { "main": main },
+    render(ctx) {
+      ctx.render();
+      const cssText = `
+        body:before {
+          content: "";
+          display: block;
+          position: absolute;
+          z-index: 9999;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: ${height};
+          background: ${color};
+          animation: ${ANIMATION};
+        }
+        @keyframes loader {
+          0% {
+            width: 10%;
+          }
+          50% {
+            width: 60%;
+          }
+          100% {
+            width: 100%;
+          }
+        }
+      `;
+      return {
+        scripts: [{ entrypoint: "main", state: {} }],
+        styles: [{ cssText, id: "__loader" }],
+      };
+    },
+  };
+}
--- templates/app/netzo.config.ts
+++ templates/app/netzo.config.ts
@@ -1,20 +1,22 @@
 import { defineNetzoConfig } from "netzo/config/mod.ts";
-import { portals } from "netzo/plugins/portals/mod.ts";
 import { api } from "netzo/plugins/api/mod.ts";
-import { createGitHubOAuthConfig } from "deno_kv_oauth/mod.ts";
 import { errorPages } from "netzo/plugins/errorPages/mod.ts";
+import { loader } from "netzo/plugins/loader/mod.ts";
+import { portals } from "netzo/plugins/portals/mod.ts";
+import { createGitHubOAuthConfig } from "deno_kv_oauth/mod.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
 export default defineNetzoConfig({
   project: "sheer-marlin-436696",
   plugins: [
+    api(),
+    errorPages(),
+    loader(),
     portals({
       email: {},
       oauth2: createGitHubOAuthConfig(),
     }),
-    api(),
-    errorPages(),
     twindPlugin(twindConfig),
   ],
 });
