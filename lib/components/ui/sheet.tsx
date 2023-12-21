847abaa44b1abb40ddcacab4b38ac9ccd1d42bee
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-12-20 19:20:37 -0600

    chore(framework): minor updates

--- lib/components/ui/sheet.tsx
+++ lib/components/ui/sheet.tsx
@@ -78,7 +78,7 @@ const SheetContent = forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="bg-inherit absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <Cross2Icon className="w-4 h-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
