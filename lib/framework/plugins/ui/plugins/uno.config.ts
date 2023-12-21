68897ff280878b0e4c901ffb238da7bef15c7cf5
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-12-21 10:22:04 -0600

    chore(framework): remove defaults in default `uno.config.ts` of `unocss` plugin

--- lib/framework/plugins/ui/plugins/uno.config.ts
+++ lib/framework/plugins/ui/plugins/uno.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "./unocss.ts";
 import presetNetzo from "./preset-netzo.ts";
 
 export default defineConfig({
-  presets: [presetNetzo({ color: "blue", radius: 0.5 })],
+  presets: [presetNetzo()],
 });
