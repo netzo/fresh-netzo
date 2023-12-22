37bdcb4a6fb2aa95a706a2d4452793843fa237d1
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-12-21 22:34:22 -0600

    chore(framework): minor improvements to `ui` plugin

--- lib/framework/plugins/ui/mod.ts
+++ lib/framework/plugins/ui/mod.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from "../../../deps/$fresh/server.ts";
 import type { NetzoConfig } from "../../../framework/mod.ts";
-import { defineConfig, unocss } from "./plugins/unocss.ts";
+import { defineConfig, unocss, type UnocssOptions } from "./plugins/unocss.ts";
 import presetNetzo from "./plugins/preset-netzo.ts";
 import _App from "./routes/_app.tsx";
 import _404 from "./routes/_404.tsx";
@@ -10,27 +10,23 @@ import _500 from "./routes/_500.tsx";
  * Plugin to add layout (nav, header, footer) and theme (colors,
  * typography, etc.) powered by UnoCSS and netzo/components.
  */
-export const ui = (options?: NetzoConfig["ui"]): Plugin => {
+export const ui = (options?: NetzoConfig["ui"] & UnocssOptions): Plugin => {
   const {
-    theme: {
-      color = "blue",
-      radius = 0.5,
-    } = {},
-  } = options ?? {} as NetzoConfig["ui"];
+    theme: { color = "blue", radius = 0.5 } = {},
+    // inlined config is used by AoT and SSR modes, however CSR mode requires injecting
+    // the inlined config via plugin `state.config` which is serialized by esbuild
+    // with the current known issue of not supporting `Function` type, so any
+    // functions (e.g. preflight.[].getCSS() functions) are dropped in serialization.
+    // Any attempt to circumvent this issue by serializing in any other way has failed,
+    // so far. Importing from a base64 encoded module works in development but fails in
+    // production (e.g. `import(`data:text/javascript;base64,${btoa(unoConfigString)}`)
+    config = defineConfig({ presets: [presetNetzo({ color, radius })] }),
+    aot = true,
+    ssr = true,
+    csr = false,
+  } = options ?? {} as NetzoConfig["ui"] & UnocssOptions;
   return {
-    ...unocss({
-      // used by AoT and SSR modes, however CSR mode requires injecting the
-      // inlined config via plugin `state.config` which is serialized by esbuild
-      // with the current known issue of not supporting `Function` type, so any
-      // functions (e.g. preflight.[].getCSS() functions) are dropped in serialization.
-      // Any attempt to circumvent this issue by serializing in any other way has failed,
-      // so far. Importing from a base64 encoded module works in development but fails in
-      // production (e.g. `import(`data:text/javascript;base64,${btoa(unoConfigString)}`)
-      config: defineConfig({ presets: [presetNetzo({ color, radius })] }),
-      aot: true,
-      ssr: true,
-      csr: true,
-    }), // { name, entrypoints, renderAsync, buildStart }
+    ...unocss({ config, aot, ssr, csr }), // { name, entrypoints, renderAsync, buildStart }
     name: "ui",
     routes: [
       { path: "/_app", component: _App },
