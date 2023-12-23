7540225acb8e55e4e105dc462a5a444f6b25cb50
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-12-23 14:03:01 -0600

    feat(components): add `tsx` utility to create Tailwind Components in one line, write less code and build faster.

--- lib/components/utils.ts
+++ lib/components/utils.ts
@@ -2,7 +2,10 @@
 // see https://github.com/unocss/unocss/issues/2748
 import { twMerge } from "../deps/tailwind-merge.ts";
 import { type ClassValue, clsx } from "../deps/clsx.ts";
+import { createTwc } from "../deps/react-twc.ts";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const twx = createTwc({ compose: cn });
---
+++ lib/deps/react-twc.ts
@@ -0,0 +1,1 @@
+export * from "https://esm.sh/v135/react-twc@1.1.0?external=react,react-dom&target=es2022";
