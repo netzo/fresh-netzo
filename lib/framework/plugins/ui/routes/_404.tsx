c23c35702d984419d0cba95f3a61a6abccadd583
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-12-24 01:23:39 -0600

    fix(framework): styles for error pages

--- lib/framework/plugins/ui/routes/_404.tsx
+++ lib/framework/plugins/ui/routes/_404.tsx
@@ -3,21 +3,19 @@ import type { NetzoState } from "../../../../framework/mod.ts";
 
 export default defineRoute<NetzoState>((_req, ctx) => {
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center">
-          <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-blue-500">
-            404
-          </h1>
-          <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-            Not Found
-          </p>
-          <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-            No matching route for{" "}
-            <code class="font-mono">{ctx.url.pathname}</code>
-          </p>
-        </div>
+    <div class="h-full grid place-items-center">
+      <div class="mx-auto max-w-screen-sm text-center">
+        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-blue-500">
+          404
+        </h1>
+        <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          Not Found
+        </p>
+        <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+          No matching route for{" "}
+          <code class="font-mono">{ctx.url.pathname}</code>
+        </p>
       </div>
-    </section>
+    </div>
   );
 });
