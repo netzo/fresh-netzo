72ddc4ce0c01801ed1e6a47ab8b50a5026efab1f
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-01-07 17:40:04 -0600

    0.3.54

--- lib/components/imports.ts
+++ lib/components/imports.ts
@@ -1,6 +1,6 @@
 export const baseImports = {
   "@/": "./",
-  "netzo/": "https://deno.land/x/netzo@0.3.53/",
+  "netzo/": "https://deno.land/x/netzo@0.3.54/",
   "$fresh/": "https://deno.land/x/fresh@1.6.1/",
   "preact": "https://esm.sh/v135/preact@10.19.2",
   "preact/": "https://esm.sh/v135/preact@10.19.2/",
