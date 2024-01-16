26322953212986279cece29f9815b9c41177aeb9
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-01-16 22:14:54 +0100

    debug(cron): disable cron module to test in production

--- templates/minimal/netzo.ts
+++ templates/minimal/netzo.ts
@@ -5,7 +5,4 @@ export const netzo = await Netzo({
   apiKey: Deno.env.get("NETZO_API_KEY")!,
 }); // [optional] loaded from .env file
 
-// NOTE: could also use the native `Deno.cron()` API instead of `netzo.cron()`
-netzo.cron("job", "*/30 * * * *", () => console.log("Runs every 30 minutes"));
-
 Deno.serve(() => Response.json({ message: "Hello World" }));
