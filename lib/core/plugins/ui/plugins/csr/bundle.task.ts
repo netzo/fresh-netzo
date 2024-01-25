504452a9c031a27c30398776d9d06e2b0ecb584c
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-01-25 21:19:59 +0100

    0.4.5

--- lib/core/plugins/ui/plugins/csr/bundle.task.ts
+++ lib/core/plugins/ui/plugins/csr/bundle.task.ts
@@ -15,7 +15,7 @@ const options: BundleOptions = {
   importMap: {
     imports: {
       "@/": "./",
-      "netzo/": "https://deno.land/x/netzo@0.4.4/",
+      "netzo/": "https://deno.land/x/netzo@0.4.5/",
       "$fresh/": "https://deno.land/x/fresh@1.6.3/",
       "preact": "https://esm.sh/v135/preact@10.19.2",
       "preact/": "https://esm.sh/v135/preact@10.19.2/",
