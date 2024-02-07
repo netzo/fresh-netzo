3bf3bdfafb363c879fb6aa54471d4140690d6774
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-07 10:44:08 +0100

    chore: solidify minimal project template

---
+++ templates/minimal/tests/test.ts
@@ -0,0 +1,17 @@
+import { fetchHtml } from "$fresh/tests/test_utils.ts";
+import { withFresh } from "./test_utils.ts";
+import { assertEquals } from "std/assert/mod.ts";
+
+Deno.test("index test", async () => {
+  await withFresh(
+    "./netzo.ts",
+    ["dev"],
+    async (address) => {
+      const doc = await fetchHtml(address);
+      assertEquals(
+        doc.querySelector("div")?.textContent,
+        "Welcome to Netzo",
+      );
+    },
+  );
+});
