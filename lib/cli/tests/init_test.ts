ef65b5517b91a8ea36c91c1b10c47df85cabe20c
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-08 08:20:12 +0100

    add test

---
+++ lib/cli/tests/init_test.ts
@@ -0,0 +1,29 @@
+export { retry } from "std/async/retry.ts";
+
+Deno.test("CLI init and task execution", async () => {
+  const tmpDirName = await Deno.makeTempDir();
+  const process = new Deno.Command(Deno.execPath(), {
+    args: [
+      "run",
+      "-A",
+      "./lib/cli/netzo.ts",
+      "init",
+      "minimal",
+      "--dir",
+      tmpDirName,
+    ],
+  }).spawn();
+  await process.status;
+
+  const checkProcess = new Deno.Command(Deno.execPath(), {
+    args: [
+      "task",
+      "ok",
+    ],
+    cwd: tmpDirName,
+  }).spawn();
+  await checkProcess.status;
+  console.log(tmpDirName);
+
+  await retry(() => Deno.remove(tmpDirName, { recursive: true }));
+});
