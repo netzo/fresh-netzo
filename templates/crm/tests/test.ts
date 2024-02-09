19370e0e71ade0c06b65704419deb08d8bbdf8b4
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-09 12:32:32 +0100

    rename files

--- templates/crm/tests/test.ts
+++ templates/crm/tests/test.ts
@@ -1,5 +1,5 @@
 import { fetchHtml } from "$fresh/tests/test_utils.ts";
-import { withFresh } from "./test_utils.ts";
+import { withFresh } from "./test.utils.ts";
 import { assertEquals } from "std/assert/mod.ts";
 
 Deno.test("index test", async () => {
