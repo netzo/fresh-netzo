2da1947569162c4bbdac3df217087b971303dcd7
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-02-09 16:53:49 +0100

    chore(templates): revert to separate `cli` and `manifest` tasks (only for `minimal` template)

--- tasks/manifest.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "std/path/mod.ts";
-import { manifest } from "$fresh/src/dev/mod.ts";
-import { type FreshConfig } from "$fresh/server.ts";
-
-const [entrypoint = "netzo.ts"] = Deno.args;
-
-const url = join(Deno.cwd(), entrypoint);
-const config: FreshConfig = (await import(url)).netzo.config;
-await manifest(Deno.cwd(), config?.router?.ignoreFilePattern);
