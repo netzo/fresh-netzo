1ca1fe9a3b56bf468ebf12e70f0fba0fd97e1ded
miguelrk <miguel.romero@netzo.io>
2024-02-14 18:01:59 +0100

    chore(deps): bump fresh to `1.6.4` (#134)

--- lib/deps/$fresh/server.ts
+++ lib/deps/$fresh/server.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/fresh@1.6.3/server.ts";
+export * from "https://deno.land/x/fresh@1.6.4/server.ts";
--- lib/deps/$fresh/src/dev/mod.ts
+++ lib/deps/$fresh/src/dev/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/fresh@1.6.3/src/dev/mod.ts";
+export * from "https://deno.land/x/fresh@1.6.4/src/dev/mod.ts";
