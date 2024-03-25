842e3dcef7574071139c2ffba1bedee422f4f005
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-03-25 17:00:51 -0600

    feat(components): add `ClientOnly` component

---
+++ lib/components/client-only.tsx
@@ -0,0 +1,6 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import type { ComponentChildren } from "preact";
+
+export const ClientOnly = ({ children }: { children: ComponentChildren }) => {
+  return IS_BROWSER ? children : null;
+};
