6733b4da525625d1294b96603e59fc6ab79fc2d0
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-03-26 03:13:31 -0600

    feat(signaldb): add `createNetzoDBAdapter` for SignalDB

---
+++ lib/integrations/databases/netzodb.signaldb.ts
@@ -0,0 +1,46 @@
+import preactReactivityAdapter from "npm:signaldb-plugin-preact@1.0.1";
+import {
+  createPersistenceAdapter,
+  PersistentCollection,
+} from "npm:signaldb@0.8.7";
+
+const createNetzoDBAdapter = <T = unknown>(name: string) => {
+  return createPersistenceAdapter({
+    async load() {
+      const response = await fetch(`/api/${name}`);
+      const items = await response.json();
+      return { items };
+    },
+    async save(_items, { added, modified, removed }) {
+      await Promise.all([
+        ...added.map(({ id, ...item }) =>
+          fetch(`/api/${name}`, {
+            method: "POST",
+            body: JSON.stringify(item),
+          })
+        ),
+        ...modified.map(({ id, ...item }) =>
+          fetch(`/api/${name}/${id}`, {
+            method: "PATCH",
+            body: JSON.stringify(item),
+          })
+        ),
+        ...removed.map(({ id }) =>
+          fetch(`/api/${name}/${id}`, {
+            method: "DELETE",
+          })
+        ),
+      ]);
+    },
+    register(_onChange) {
+      return Promise.resolve();
+    },
+  });
+};
+
+export const createCollection = <T = unknown>(name: string) => {
+  return new PersistentCollection<T>(name, {
+    reactivity: preactReactivityAdapter,
+    persistence: createNetzoDBAdapter(name),
+  });
+};
