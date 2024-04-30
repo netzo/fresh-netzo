90d318a15e685ebd711a6d28e1f5c9d4b9888d12
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-04-30 10:25:43 +0200

    fix(plugins/loader): typo in filename

---
+++ lib/plugins/loader/plugin.ts
@@ -0,0 +1,75 @@
+// see https://github.com/nesterow/tailored#preloader-plugin
+import { Plugin } from "$fresh/server.ts";
+import type { NetzoState } from "netzo/config/mod.ts";
+
+export type LoaderOptions = {
+  color?: string;
+  height?: string;
+};
+
+// deno-lint-ignore ban-types
+export type LoaderState = {};
+
+const COLOR = "hsl(var(--primary) / var(--un-bg-opacity))";
+
+const ANIMATION = "loader 12s ease infinite";
+
+export const loader = ({
+  color = `linear-gradient(to right, ${COLOR} 0%, ${COLOR} 51%, ${COLOR} 100%)`,
+  height = "4px",
+}: LoaderOptions = {}): Plugin<NetzoState> => {
+  const main = `data:application/javascript,
+  export default function(_) {
+    const loader = document.getElementById("__loader");
+    const rules = loader.sheet.rules[0];
+    window.showLoader = () => {
+      rules.style.display = "block";
+      rules.style.animation = "${ANIMATION}";
+    };
+    window.hideLoader = () => {
+      rules.style.animationDuration = ".3s";
+      setTimeout(() => {
+        rules.style.display = "none";
+        rules.style.animation = "none";
+      }, 300);
+    };
+    window.addEventListener('load', hideLoader);
+  }
+  `;
+  return {
+    name: "loader",
+    entrypoints: { main },
+    render(ctx) {
+      ctx.render();
+      const cssText = `
+        body:before {
+          content: "";
+          display: block;
+          position: absolute;
+          z-index: 9999;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: ${height};
+          background: ${color};
+          animation: ${ANIMATION};
+        }
+        @keyframes loader {
+          0% {
+            width: 10%;
+          }
+          50% {
+            width: 60%;
+          }
+          100% {
+            width: 100%;
+          }
+        }
+      `;
+      return {
+        scripts: [{ entrypoint: "main", state: {} }],
+        styles: [{ cssText, id: "__loader" }],
+      };
+    },
+  };
+};
--- lib/plugins/mod.ts
+++ lib/plugins/mod.ts
@@ -1,6 +1,7 @@
 export * from "./auth/plugin.ts";
 export * from "./cron/plugin.ts";
 export * from "./environments/plugin.ts";
+export * from "./loader/plugin.ts";
 export * from "./mdx/plugin.ts";
 export * from "./rest/plugin.ts";
 export * from "./unocss/plugin.ts";
