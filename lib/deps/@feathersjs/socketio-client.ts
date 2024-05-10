5e0c72e5fd60b8d27c2c3e4f09ddaebc6f310316
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-05-10 18:11:37 +0200

    chore(deps): bump socketio deps

--- lib/deps/@feathersjs/socketio-client.ts
+++ lib/deps/@feathersjs/socketio-client.ts
@@ -1,1 +1,1 @@
-export { default as socketio } from "npm:@feathersjs/socketio-client@5.0.6";
+export { default as socketio } from "npm:@feathersjs/socketio-client@5.0.24";
--- lib/deps/socket.io-client.ts
+++ lib/deps/socket.io-client.ts
@@ -1,1 +1,1 @@
-export { default as io } from "npm:socket.io-client@4.7.1";
+export { default as io } from "npm:socket.io-client@4.7.5";
