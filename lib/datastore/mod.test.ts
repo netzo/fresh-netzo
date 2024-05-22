05fbf0dad5036ba54eb38c9945ae5862735370c7
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-05-22 23:53:20 +0200

    chore(datastore): update test

--- lib/datastore/mod.test.ts
+++ lib/datastore/mod.test.ts
@@ -1,11 +1,11 @@
 import { assertExists } from "../deps/std/assert/mod.ts";
 import { datastore } from "./mod.ts";
 
-const db = datastore({ kv: await Deno.openKv(":memory:") });
+const ds = datastore({ kv: await Deno.openKv(":memory:") });
 
 Deno.test("[datastore] datastore", { ignore: false }, async (t) => {
   await t.step("declarations", () => {
     assertExists(datastore);
-    assertExists(db);
+    assertExists(ds);
   });
 });
