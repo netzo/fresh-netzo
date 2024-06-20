85ef1004030c562d1753bf1c6da824c8277b1dc6
Miguel Romero Karam <miguelromerokaram@gmail.com>
2024-06-20 11:10:25 -0600

    0.5.85

--- lib/deps/@radix-ui/react-slot.ts
+++ lib/deps/@radix-ui/react-slot.ts
@@ -1,1 +1,1 @@
-export * from "https://esm.sh/@radix-ui/react-slot@latest?external=react,react-dom&target=es2022";
+export * from "https://esm.sh/@radix-ui/react-slot@1.0.2?external=react,react-dom&target=es2022";
